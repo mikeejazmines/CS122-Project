dceba0971b4410c34cb969b2a7139940b5b785e7
Luis Ligunas <luisrainier.ligunas@gmail.com>
2017-05-03 10:16:28 +0800

    Initial commit

---
+++ report.js
@@ -0,0 +1,16 @@
+document.getElementById("dayB").onclick = function() {clicked(0)};
+document.getElementById("weekB").onclick = function() {clicked(1)};
+document.getElementById("monthB").onclick = function() {clicked(2)};
+document.getElementById("yearB").onclick = function() {clicked(3)};
+
+function clicked(x){
+	var buttons = ["day", "week", "month", "year"];
+
+	for(i=0; i<buttons.length; i++){
+		var state = "none"
+        if(i==x){
+        	state = "initial";
+    	}
+        document.getElementById(buttons[i]).style.display = state;
+	}
+}
