de7f9f6a24b199d14a896721ed692d1e5b25cccd
Albdog <joaquinjacinto11@gmail.com>
2017-05-03 10:51:19 +0800

    sql file setup
    still empty will update after normalization is done

---
+++ jamal-setup.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE jamaldb;
+USE jamalbd;
+
+CREATE TABLE order_item (
+
+);
+
+CREATE TABLE customer (
+
+);
+
+CREATE TABLE sales_agent (
+
+);
+
+CREATE TABLE order (
+
+);
+
+CREATE TABLE delivery (
+
+);
+
+CREATE TABLE inventory (
+
+);
+
+CREATE TABLE item (
+
+);
+
+CREATE TABLE folder (
+
+);
+
+CREATE TABLE pen_organizers (
+
+);
+
+CREATE TABLE planners (
+
+);
